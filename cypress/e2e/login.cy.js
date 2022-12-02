cb5e97ae69c69d8df8a07d6179856e0064db421c
Viki <viki.verebes90@gmail.com>
2022-12-02 12:41:14 +0100

    some changes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,7 +4,6 @@ describe("login test", () => {
     it("login with empty email field", () => {
         cy.visit("https://gallery-app.vivifyideas.com/");
         cy.get('a[class="nav-link nav-buttons"]').first().click();
-        cy.get("#email").type("");
         cy.get("#password").type("12341234");
         cy.get("button").click();
     });
