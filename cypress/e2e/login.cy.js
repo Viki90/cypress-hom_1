f310f7f8c0fb334be40559383a8127b9d707c4c7
Viki <viki.verebes90@gmail.com>
2022-12-02 14:15:16 +0100

    login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,8 +2,41 @@
 
 describe("login test", () => {
     it("login with empty email field", () => {
-        cy.visit("https://gallery-app.vivifyideas.com/");
+        cy.visit("/");
         cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get("#email");
+        cy.get("#password").type("12341234");
+        cy.get("button").click();
+    });
+
+    it("login with less then 8 characters in password", () => {
+        cy.visit("/");
+        cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get("#email").type("marci@gmail.com");
+        cy.get("#password").type("1234");
+        cy.get("button").click();
+    });
+
+    it("login with invalid credentials", () => {
+        cy.visit("/");
+        cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get("#email").type("marci2222222@gmail.com");
+        cy.get("#password").type("12341234");
+        cy.get("button").click();
+    });
+
+    it("login with invalid email format", () => {
+        cy.visit("/");
+        cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get("#email").type("marci.com");
+        cy.get("#password").type("12341234");
+        cy.get("button").click();
+    });
+
+    it("invalid email address without domain", () => {
+        cy.visit("/");
+        cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get("#email").type("marci@");
         cy.get("#password").type("12341234");
         cy.get("button").click();
     });
